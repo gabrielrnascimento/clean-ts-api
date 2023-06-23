e887975b843b35d243f8a0ca44203fff61c29a6c
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-06-23 00:05:40 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,6 +1,6 @@
 import { InvalidParamError, MissingParamError } from '../../errors';
 import { badRequest } from '../../helpers/http-helper';
-import { type EmailValidator } from '../../protocols';
+import { type HttpRequest, type EmailValidator } from '../../protocols';
 import { LoginController } from './login';
 
 const makeEmailValidator = (): EmailValidator => {
@@ -11,6 +11,13 @@ const makeEmailValidator = (): EmailValidator => {
   }
   return new EmaiLValidatorStub();
 };
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    email: 'any_email@mail.com',
+    password: 'any_password'
+  }
+});
 
 interface SutTypes {
   sut: LoginController
@@ -52,27 +59,14 @@ describe('LoginController', () => {
   test('should return 400 if an invalid email is provided', async () => {
     const { sut, emailValidatorStub } = makeSut();
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
-
-    const httpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password'
-      }
-    };
-    const HttpResponse = await sut.handle(httpRequest);
+    const HttpResponse = await sut.handle(makeFakeRequest());
     expect(HttpResponse).toEqual(badRequest(new InvalidParamError('email')));
   });
 
   test('should call EmailValidator with correct email', async () => {
     const { sut, emailValidatorStub } = makeSut();
     const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid');
-    const httpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password'
-      }
-    };
-    await sut.handle(httpRequest);
+    await sut.handle(makeFakeRequest());
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com');
   });
 });
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -6,9 +6,10 @@ export class LoginController implements Controller {
   constructor (private readonly emaiLValidator: EmailValidator) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.email) return await new Promise(resolve => { resolve(badRequest(new MissingParamError('email'))); });
-    if (!httpRequest.body.password) return await new Promise(resolve => { resolve(badRequest(new MissingParamError('password'))); });
-    const isValid = this.emaiLValidator.isValid(httpRequest.body.email);
+    const { email, password } = httpRequest.body;
+    if (!email) return await new Promise(resolve => { resolve(badRequest(new MissingParamError('email'))); });
+    if (!password) return await new Promise(resolve => { resolve(badRequest(new MissingParamError('password'))); });
+    const isValid = this.emaiLValidator.isValid(email);
     if (!isValid) return await new Promise(resolve => { resolve(badRequest(new InvalidParamError('email'))); });
     return null as unknown as HttpResponse;
   }
