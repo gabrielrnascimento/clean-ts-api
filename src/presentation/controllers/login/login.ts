1a4b0b96f2a130cf2d1b946b9fa6e3b331660dcf
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-07-18 01:50:47 -0300

    feat: add LoginValidationFactory

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,22 +1,19 @@
-import { MissingParamError, InvalidParamError } from '../../errors';
 import { badRequest, unauthorized, serverError, ok } from '../../helpers/http-helper';
-import { type Authentication, type Controller, type EmailValidator, type HttpRequest, type HttpResponse } from './login-protocols';
+import { type Validation, type Authentication, type Controller, type HttpRequest, type HttpResponse } from './login-protocols';
 
 export class LoginController implements Controller {
   constructor (
-    private readonly emaiLValidator: EmailValidator,
-    private readonly authentication: Authentication
+    private readonly authentication: Authentication,
+    private readonly validation: Validation
   ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredField = ['email', 'password'];
-      for (const field of requiredField) {
-        if (!httpRequest.body[field]) return badRequest(new MissingParamError(field));
+      const error = this.validation.validate(httpRequest.body);
+      if (error) {
+        return badRequest(error);
       }
       const { email, password } = httpRequest.body;
-      const isValid = this.emaiLValidator.isValid(email);
-      if (!isValid) return badRequest(new InvalidParamError('email'));
       const accessToken = await this.authentication.auth(email, password);
       if (!accessToken) return unauthorized();
       return ok({ accessToken });
--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,6 +1,5 @@
 /* istanbul ignore next */
 export * from '../../protocols';
-export * from '../../protocols/email-validator';
 export * from '../../../domain/models/account';
 export * from '../../../domain/usecases/add-account';
 export * from '../../helpers/validators/validation';
