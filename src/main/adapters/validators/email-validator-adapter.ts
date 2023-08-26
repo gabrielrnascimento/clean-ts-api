734f8b26e6bf66cbf5f3088cb5eca218b445f544
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-26 11:47:02 -0300

    refactor: move EmailValidatorAdapter to main layer

--- src/main/adapters/validators/email-validator-adapter.ts
+++ src/main/adapters/validators/email-validator-adapter.ts
@@ -1,5 +1,5 @@
-import { type EmailValidator } from '../presentation/protocols';
 import validator from 'validator';
+import { type EmailValidator } from '../../../presentation/protocols';
 
 export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
