cb36e70862ff1eb930b6b7253ed6eb092ea6685c
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-26 11:40:19 -0300

    refactor: remove duplicated code

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { type Encrypter } from '../../../data/protocols/cryptography/token-generator';
 
 export class JwtAdapter implements Encrypter {
-  constructor (private readonly secret: string) {}
+  constructor (private readonly secret: string) { }
 
   async encrypt (value: string): Promise<string> {
     const accessToken = jwt.sign({ id: value }, this.secret);
--- src/presentation/helpers/validators/required-field-validation.ts
+++ src/presentation/helpers/validators/required-field-validation.ts
@@ -2,9 +2,7 @@ import { MissingParamError } from '../../errors';
 import { type Validation } from '../../protocols/validation';
 
 export class RequiredFieldValidation implements Validation {
-  constructor (private readonly fieldName: string) {
-    this.fieldName = fieldName;
-  }
+  constructor (private readonly fieldName: string) { }
 
   validate (input: any): Error {
     if (!input[this.fieldName]) return new MissingParamError(this.fieldName);
