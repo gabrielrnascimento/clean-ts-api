905087505debef83f0ebb189a8a009a7dbce0834
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-25 21:04:23 -0300

    test: ensure JwtAdapter returns a token on sign success

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,6 +1,10 @@
 import jwt from 'jsonwebtoken';
 import { JwtAdapter } from './jwt-adapter';
 
+jest.mock('jsonwebtoken', () => ({
+  sign (): string { return 'any_token'; }
+}));
+
 describe('JwtAdapter', () => {
   test('should call sign with correct values', async () => {
     const sut = new JwtAdapter('secret');
@@ -9,5 +13,13 @@ describe('JwtAdapter', () => {
     await sut.encrypt('any_id');
 
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
+  });
+
+  test('should return a token on sign success', async () => {
+    const sut = new JwtAdapter('secret');
+
+    const accessToken = await sut.encrypt('any_id');
+
+    expect(accessToken).toBe('any_token');
   });
 });
