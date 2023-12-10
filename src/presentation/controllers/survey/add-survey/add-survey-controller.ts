52fd73cbd34132d65a93156613d1cce564012422
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-12-10 11:33:28 -0300

    feat: ensure AddSurveyController returns 400 if Validation fails

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '../../access/signup/signup-controller-protocols';
 import { type HttpResponse, type Controller, type HttpRequest, type Validation } from './add-survey-controller-protocols';
 
 export class AddSurveyController implements Controller {
@@ -6,7 +7,8 @@ export class AddSurveyController implements Controller {
   ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body);
+    const error = this.validation.validate(httpRequest.body);
+    if (error) return badRequest(error);
     return null;
   }
 }
