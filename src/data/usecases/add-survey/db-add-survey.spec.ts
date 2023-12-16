3fd7c8c3ffa8e8f0ea04db23081ea63f1a8df740
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-12-16 11:35:41 -0300

    refactor: change AddSurvey to insert current date

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -1,12 +1,14 @@
 import { DbAddSurvey } from './db-add-survey';
 import { type AddSurveyRepository, type AddSurveyModel } from './db-add-survey-protocols';
+import MockDate from 'mockdate';
 
 const makeFakeSurveyData = (): AddSurveyModel => ({
   question: 'any_question',
   answers: [{
     image: 'any_image',
     answer: 'any_answer'
-  }]
+  }],
+  date: new Date()
 });
 
 const makeAddSurveyRepository = (): AddSurveyRepository => {
@@ -32,6 +34,14 @@ const makeSut = (): SutTypes => {
 };
 
 describe('DbAddSurvey', () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  beforeAll(() => {
+    MockDate.reset();
+  });
+
   test('should call AddSurveyRepository with correct values', async () => {
     const { sut, addSurveyRepositoryStub } = makeSut();
     const addSpy = jest.spyOn(addSurveyRepositoryStub, 'add');
--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -34,7 +34,8 @@ describe('SurveyMongoRepository', () => {
         {
           answer: 'other_answer'
         }
-      ]
+      ],
+      date: new Date()
     });
     const survey = await surveyCollection.findOne({ question: 'any_question' });
     expect(survey).toBeTruthy();
