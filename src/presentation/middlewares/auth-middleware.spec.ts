cd0f668884c57f8e67a4e41e6f61c19244174199
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-12-16 10:35:08 -0300

    test: ensure AuthMiddleware returns 403 if LoadAccountByToken retuns null

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -54,4 +54,12 @@ describe('AuthMiddleware', () => {
     await sut.handle(makeFakeRequest());
     expect(loadSpy).toHaveBeenCalledWith('any_token');
   });
+
+  test('should return 403 if LoadAccountByToken returns null', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut();
+    jest.spyOn(loadAccountByTokenStub, 'load')
+      .mockReturnValueOnce(Promise.resolve(null));
+    const httpResponse = await sut.handle({});
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
+  });
 });
