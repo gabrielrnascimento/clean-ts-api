25d8c9af8a9d8be8fcd4d3dce66a4138b9a26ad1
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-12-16 13:38:05 -0300

    test: ensure DbLoadSurveys throws if LoadSurveysRepository throws

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -53,6 +53,14 @@ describe('DbLoadSurveys', () => {
     expect(loadAllSpy).toHaveBeenCalled();
   });
 
+  test('should throw if LoadSurveysRepository throws', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut();
+    jest.spyOn(loadSurveysRepositoryStub, 'loadAll')
+      .mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()); }));
+    const promise = sut.load();
+    await expect(promise).rejects.toThrow();
+  });
+
   test('should return a list of Surveys on success', async () => {
     const { sut } = makeSut();
     const surveys = await sut.load();
