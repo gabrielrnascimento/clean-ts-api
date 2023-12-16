aa8a7f6913dbd1248903436e479bbf4508908065
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-12-16 12:25:49 -0300

    feat: ensure LoadSurveysController returns 500 if LoadSurveys throws

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,10 +1,14 @@
-import { type LoadSurveys, type Controller, type HttpRequest, type HttpResponse, ok } from './load-surveys-controller-protocols';
+import { type LoadSurveys, type Controller, type HttpRequest, type HttpResponse, ok, serverError } from './load-surveys-controller-protocols';
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    const surveys = await this.loadSurveys.load();
-    return ok(surveys);
+    try {
+      const surveys = await this.loadSurveys.load();
+      return ok(surveys);
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
