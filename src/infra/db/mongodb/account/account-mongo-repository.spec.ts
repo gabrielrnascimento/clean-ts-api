8e08611496b465397000821fd5b945b46ccd29c1
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-12-16 10:35:22 -0300

    test: ensure AccountMongoRepository returns null if loadByToken fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -124,5 +124,11 @@ describe('AccountMongoRepository', () => {
       expect(account.email).toBe('any_email@mail.com');
       expect(account.password).toBe('any_password');
     });
+
+    test('should return null if loadByToken fails', async () => {
+      const sut = makeSut();
+      const account = await sut.loadByToken('any_token');
+      expect(account).toBeFalsy();
+    });
   });
 });
