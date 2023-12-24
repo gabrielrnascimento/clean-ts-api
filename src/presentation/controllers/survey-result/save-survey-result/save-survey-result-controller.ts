561172632e40d552e955c43afb2f84e15038111b
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-12-24 11:01:10 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,18 +1,31 @@
-import { type HttpResponse, type Controller, type HttpRequest, type LoadSurveyById, forbidden, InvalidParamError, serverError } from './save-survey-result-controller-protocols';
+import { type HttpResponse, type Controller, type HttpRequest, type LoadSurveyById, forbidden, InvalidParamError, serverError, type SaveSurveyResult } from './save-survey-result-controller-protocols';
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params;
       const { answer } = httpRequest.body;
+      const { accountId } = httpRequest;
+
       const survey = await this.loadSurveyById.loadById(surveyId);
-      if (survey) {
-        const answers = survey.answers.map(answer => answer.answer);
-        if (!answers.includes(answer)) return forbidden(new InvalidParamError('answer'));
+      if (!survey) {
+        return forbidden(new InvalidParamError('surveyId'));
       }
-      return forbidden(new InvalidParamError('surveyId'));
+
+      const answers = survey.answers.map(answer => answer.answer);
+      if (!answers.includes(answer)) return forbidden(new InvalidParamError('answer'));
+
+      await this.saveSurveyResult.save({
+        surveyId,
+        accountId,
+        answer,
+        date: new Date()
+      });
     } catch (error) {
       return serverError(error);
     }
