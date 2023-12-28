a50c91e3e01bf34aa5a60c0116027718faff9a08
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-12-28 00:14:57 -0300

    feat: ensure LoadSurveyResultController calls LoadSurveyResult with correct values

--- src/domain/test/mock-survey-result.ts
+++ src/domain/test/mock-survey-result.ts
@@ -1,5 +1,6 @@
 import { type SurveyResultModel } from '@/domain/models/survey-result';
 import { type SaveSurveyResult, type SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result';
+import { type LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result';
 
 export const mockSaveSurveyResultParams = (): SaveSurveyResultParams => ({
   accountId: 'any_account_id',
@@ -47,4 +48,13 @@ export const mockSaveSurveyResult = (): SaveSurveyResult => {
     }
   }
   return new SaveSurveyResultStub();
+};
+
+export const mockLoadSurveyResult = (): LoadSurveyResult => {
+  class LoadSurveyResultStub implements LoadSurveyResult {
+    async load (surveyId: string): Promise<SurveyResultModel> {
+      return await Promise.resolve(mockSurveyResultModel());
+    }
+  }
+  return new LoadSurveyResultStub();
 };
--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,14 +1,19 @@
-import { type HttpRequest, type Controller, type HttpResponse, type LoadSurveyById, forbidden, InvalidParamError, serverError } from './load-survey-result-controller-protocols';
+import { type HttpRequest, type Controller, type HttpResponse, type LoadSurveyById, forbidden, InvalidParamError, serverError, type LoadSurveyResult } from './load-survey-result-controller-protocols';
 
 export class LoadSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) { };
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly loadSurveyResult: LoadSurveyResult
+  ) { };
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
     try {
-      const survey = await this.loadSurveyById.loadById(request.params.surveyId);
+      const surveyId = request.params.surveyId;
+      const survey = await this.loadSurveyById.loadById(surveyId);
       if (!survey) {
         return forbidden(new InvalidParamError('surveyId'));
       }
+      await this.loadSurveyResult.load(surveyId);
     } catch (error) {
       return serverError(error);
     }
