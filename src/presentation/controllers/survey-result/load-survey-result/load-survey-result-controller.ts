a2dca35b92e73c844d6554e485fcb8898f7a0638
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-12-27 23:59:45 -0300

    feat: ensure LoadSurveyResultController returns 403 if LoadSurveyById returns null

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,10 +1,12 @@
-import { type HttpRequest, type Controller, type HttpResponse, type LoadSurveyById } from './load-survey-result-controller-protocols';
+import { type HttpRequest, type Controller, type HttpResponse, type LoadSurveyById, forbidden, InvalidParamError } from './load-survey-result-controller-protocols';
 
 export class LoadSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) { };
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveyById.loadById(request.params.surveyId);
-    return null;
+    const survey = await this.loadSurveyById.loadById(request.params.surveyId);
+    if (!survey) {
+      return forbidden(new InvalidParamError('surveyId'));
+    }
   }
 }
