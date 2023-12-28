790fc9d09657d3fa74f8cbfbd23c844ebede5f03
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-12-28 00:03:36 -0300

    feat: ensure LoadSurveyResultController returns 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,12 +1,16 @@
-import { type HttpRequest, type Controller, type HttpResponse, type LoadSurveyById, forbidden, InvalidParamError } from './load-survey-result-controller-protocols';
+import { type HttpRequest, type Controller, type HttpResponse, type LoadSurveyById, forbidden, InvalidParamError, serverError } from './load-survey-result-controller-protocols';
 
 export class LoadSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) { };
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    const survey = await this.loadSurveyById.loadById(request.params.surveyId);
-    if (!survey) {
-      return forbidden(new InvalidParamError('surveyId'));
+    try {
+      const survey = await this.loadSurveyById.loadById(request.params.surveyId);
+      if (!survey) {
+        return forbidden(new InvalidParamError('surveyId'));
+      }
+    } catch (error) {
+      return serverError(error);
     }
   }
 }
