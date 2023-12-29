a4936e80d989138c46f68dfbe0ecc96415b61097
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-12-29 10:57:34 -0300

    feat: add didAnswer field to LoadSurveys

--- src/data/usecases/survey/load-surveys/db-load-surveys.ts
+++ src/data/usecases/survey/load-surveys/db-load-surveys.ts
@@ -2,8 +2,9 @@ import { type LoadSurveys, type LoadSurveysRepository, type SurveyModel } from '
 
 export class DbLoadSurveys implements LoadSurveys {
   constructor (private readonly loadSurveysRepository: LoadSurveysRepository) {}
-  async load (): Promise<SurveyModel[]> {
-    const surveys = await this.loadSurveysRepository.loadAll();
+
+  async load (accountId: string): Promise<SurveyModel[]> {
+    const surveys = await this.loadSurveysRepository.loadAll(accountId);
     return surveys;
   }
 }
