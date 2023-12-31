de9ad4fb1ac5417c9ace0989aab13069a4c014d9
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-12-31 13:17:39 -0300

    test: ensure Login Query returns an account on valid credentials

---
+++ tests/graphql/login.test.ts
@@ -0,0 +1,55 @@
+import app from '@/main/config/app';
+import { MongoHelper } from '@/infra/db/mongodb';
+import { hash } from 'bcrypt';
+import request from 'supertest';
+import { type Collection } from 'mongodb';
+
+let accountCollection: Collection;
+
+describe('Login GraphQL', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+
+  describe('Login Query', () => {
+    const generateQuery = (email: string, password: string): string => `
+      query {
+        login(email: "${email}", password: "${password}") {
+          accessToken
+          name
+          }
+        }`;
+
+    const email = 'gabriel@mail.com';
+    const password = '123';
+    const name = 'Gabriel';
+
+    const query = generateQuery(email, password);
+
+    test('should return an account on valid credentials', async () => {
+      const hashedPassword = await hash(password, 12);
+      await accountCollection.insertOne({
+        name,
+        email,
+        password: hashedPassword
+      });
+
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query });
+
+      expect(res.status).toBe(200);
+      expect(res.body.data.login.accessToken).toBeTruthy();
+      expect(res.body.data.login.name).toBe(name);
+    });
+  });
+});
