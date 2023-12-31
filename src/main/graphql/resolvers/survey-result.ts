5c45cbd65d2fb336671ad11d30918ff701c55501
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-12-31 12:45:47 -0300

    feat: add survey result query and mutation

---
+++ src/main/graphql/resolvers/survey-result.ts
@@ -0,0 +1,16 @@
+import { adaptResolver } from '@/main/adapters';
+import { makeLoadSurveyResultController, makeSaveSurveyResultController } from '@/main/factories/controllers';
+
+export default {
+  Query: {
+    surveyResult: async (parent: any, args: any): Promise<any> => {
+      return await adaptResolver(makeLoadSurveyResultController(), args);
+    }
+  },
+
+  Mutation: {
+    saveSurveyResult: async (parent: any, args: any): Promise<any> => {
+      return await adaptResolver(makeSaveSurveyResultController(), args);
+    }
+  }
+};
