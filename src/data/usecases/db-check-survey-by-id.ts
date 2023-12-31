15a75fabe408f73f387244dcda08a5bc132e5f39
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-12-31 12:23:26 -0300

    style: fix typo

--- src/data/usecases/db-check-survey-by-id.ts
+++ src/data/usecases/db-check-survey-by-id.ts
@@ -2,9 +2,9 @@ import { type CheckSurveyById } from '@/domain/usecases';
 import { type CheckSurveyByIdRepository } from '../protocols/db/survey';
 
 export class DbCheckSurveyById implements CheckSurveyById {
-  constructor (private readonly checkurveyByIdRepository: CheckSurveyByIdRepository) {}
+  constructor (private readonly checkSurveyByIdRepository: CheckSurveyByIdRepository) {}
 
   async checkById (id: string): Promise<CheckSurveyById.Result> {
-    return await this.checkurveyByIdRepository.checkById(id);
+    return await this.checkSurveyByIdRepository.checkById(id);
   }
 }
